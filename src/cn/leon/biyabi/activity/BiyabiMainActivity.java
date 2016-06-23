ec0799b6ea570dd922621c66afdbe2f49730d67a
leonFighting <379029170@qq.com>
2016-06-23 16:28:03 +0800

    完成主界面和闪屏界面布局

---
+++ src/cn/leon/biyabi/activity/BiyabiMainActivity.java
@@ -0,0 +1,15 @@
+package cn.leon.biyabi.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.leon.biyabi.R;
+
+public class BiyabiMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biyabi_main);
+    }
+}
