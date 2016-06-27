975728e7c14274685b8cd3a7e69a1d3b7cbb6480
leonFighting <379029170@qq.com>
2016-06-27 20:15:00 +0800

    新增FooterViewHolder

---
+++ src/cn/ucai/fulicenter/widget/FooterViewHolder.java
@@ -0,0 +1,41 @@
+package cn.ucai.fulicenter.widget;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by leon on 16/6/21.
+ *
+ */
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG
+*/
+public class FooterViewHolder extends RecyclerView.ViewHolder {
+    public TextView tvFooter;
+    public FooterViewHolder(View itemView) {
+        super(itemView);
+        tvFooter = (TextView) itemView.findViewById(R.id.tv_footer);
+    }
+}
