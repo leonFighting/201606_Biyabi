f7e7e1e8958e34c8a0ece758f5d2715281f84ebd
leonFighting <379029170@qq.com>
2016-06-27 15:35:32 +0800

    修复v4包冲突，完成主界面菜单切换

--- src/cn/ucai/fulicenter/activity/BiyabiMainActivity.java
+++ src/cn/ucai/fulicenter/activity/BiyabiMainActivity.java
@@ -1,15 +1,101 @@
 package cn.ucai.fulicenter.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.fragment.SpecialGoodFragment;
 
-public class BiyabiMainActivity extends Activity {
+public class BiyabiMainActivity extends BaseActivity {
+    public static final String TAG = BiyabiMainActivity.class.getName();
+    SpecialGoodFragment mSpecialGoodFragment;
+
+    private Fragment[] mFragments = new Fragment[1];
+    private int index;
+    private int currentTabIndex;
+
+    TextView mtvCartHint;
+    RadioButton mRadioHome;
+    RadioButton mRadioSearch;
+    RadioButton mRadioShareorder;
+    RadioButton mRadioCart;
+    RadioButton mRadioUser;
+
+    private RadioButton[] mRadios = new RadioButton[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biyabi_main);
+        initView();
+        initFragment();
+    }
+
+    private void initFragment() {
+        mSpecialGoodFragment = new SpecialGoodFragment();
+        mFragments[0] = mSpecialGoodFragment;
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, mSpecialGoodFragment)
+                .show(mSpecialGoodFragment)
+                .commit();
+    }
+
+    private void initView() {
+        mtvCartHint = (TextView) findViewById(R.id.tv_CartHint);
+
+        mRadioHome = (RadioButton) findViewById(R.id.layout_home);
+        mRadioSearch = (RadioButton) findViewById(R.id.layout_search);
+        mRadioShareorder = (RadioButton) findViewById(R.id.layout_shareorder);
+        mRadioCart = (RadioButton) findViewById(R.id.layout_cart);
+        mRadioUser = (RadioButton) findViewById(R.id.layout_user);
+
+        mRadios[0] = mRadioHome;
+        mRadios[1] = mRadioSearch;
+        mRadios[2] = mRadioShareorder;
+        mRadios[3] = mRadioCart;
+        mRadios[4] = mRadioUser;
+    }
+
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.layout_home:
+                index = 0;
+                break;
+            case R.id.layout_search:
+                index = 1;
+                break;
+            case R.id.layout_shareorder:
+                index = 2;
+                break;
+            case R.id.layout_cart:
+                index = 3;
+                break;
+            case R.id.layout_user:
+                index = 4;
+                break;
+
+        }
+        if (currentTabIndex != index) {
+            setRadioChecked(index);
+            currentTabIndex = index;
+        }
+    }
+
+    public void setRadioChecked(int index) {
+        for (int i = 0; i < mRadios.length; i++) {
+            if (i == index) {
+                mRadios[i].setChecked(true);
+            } else {
+                mRadios[i].setChecked(false);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
